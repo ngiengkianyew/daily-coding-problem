de5048c39c3041704d5cbc9eae6523d43af273d5
Vineet John <iamvineetjohn@gmail.com>
2018-03-19 13:08:14 -0400

    Add solution to Problem 2

--- README.md
+++ README.md
@@ -2,10 +2,27 @@
 
 Solutions to problems sent by dailycodingproblem.com
 
+---
 
 #### Problem 1
+
 Given a list of numbers, return whether any two sums to k.
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
 Bonus: Can you do this in one pass?
 
 [Solution](solutions/problem_1.py)
+
+---
+
+#### Problem 2
+
+This problem was asked by Uber.
+
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+
+[Solution](solutions/problem_2.py)
---
+++ solutions/problem_2.py
@@ -0,0 +1,30 @@
+def get_factors(array):
+    cumulative_product = 1
+    right_prod_array = list()
+    for num in array:
+        cumulative_product *= num
+        right_prod_array.append(cumulative_product)
+
+    cumulative_product = 1
+    left_prod_array = list()
+    for num in array[::-1]:
+        cumulative_product *= num
+        left_prod_array.append(cumulative_product)
+    left_prod_array = left_prod_array[::-1]
+
+    output_array = list()
+    for i in range(len(array)):
+        num = None
+        if i == 0:
+            num = left_prod_array[i + 1]
+        elif i == len(array) - 1:
+            num = right_prod_array[i - 1]
+        else:
+            num = right_prod_array[i - 1] * left_prod_array[i + 1]
+        output_array.append(num)
+    
+    return output_array
+
+
+assert get_factors([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24]
+assert get_factors([3, 2, 1]) == [2, 3, 6]
