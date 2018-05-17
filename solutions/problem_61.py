90aa55e074093c9d9392cbae584ece0dfb87d520
Vineet John <iamvineetjohn@gmail.com>
2018-05-17 17:23:31 -0400

    Improve solution to Problem 61

--- solutions/problem_61.py
+++ solutions/problem_61.py
@@ -11,12 +11,11 @@ def pow(x, y):
     result = x
     while current_pow <= y_abs:
         prev_result = result
-        result *= result
         prev_pow = current_pow
+        result *= result
         current_pow *= 2
 
-    if prev_pow:
-        prev_result *= pow(x, y - prev_pow)
+    prev_result *= pow(x, y_abs - prev_pow)
 
     return 1/prev_result if y != y_abs else prev_result
 
@@ -26,3 +25,7 @@ assert pow(2, 10) == 1024
 assert pow(2, 1) == 2
 assert pow(3, 3) == 27
 assert pow(10, 3) == 1000
+assert pow(2, -3) == 0.125
+assert pow(10, -2) == 0.01
+assert pow(5, 0) == 1
+assert pow(0, 2) == 0
