b547d7904e64e6297964dc1329fc919e5a5e9086
Vineet John <iamvineetjohn@gmail.com>
2018-05-20 13:24:32 -0400

    Add naive solution to Problem 64

--- README.md
+++ README.md
@@ -966,3 +966,15 @@ and the target word 'FOAM', you should return true, since it's the leftmost colu
 [Solution](solutions/problem_63.py)
 
 ---
+
+#### Problem 64
+
+This problem was asked by Google.
+
+A knight's tour is a sequence of moves by a knight on a chessboard such that all squares are visited once.
+
+Given N, write a function to return the number of knight's tours on an N by N chessboard.
+
+[Solution](solutions/problem_64.py)
+
+---
---
+++ solutions/problem_64.py
@@ -0,0 +1,72 @@
+import random
+
+
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __hash__(self):
+        return hash((self.x, self.y))
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __repr__(self):
+        return "Position=[x={},y={}]".format(self.x, self.y)
+
+
+def get_potential_knight_moves(start, size, visited):
+    moves = list()
+    moves.append(Position(start.x + 1, start.y + 2))
+    moves.append(Position(start.x + 1, start.y - 2))
+    moves.append(Position(start.x - 1, start.y + 2))
+    moves.append(Position(start.x - 1, start.y - 2))
+    moves.append(Position(start.x + 2, start.y + 1))
+    moves.append(Position(start.x + 2, start.y - 1))
+    moves.append(Position(start.x - 2, start.y + 1))
+    moves.append(Position(start.x - 2, start.y - 1))
+
+    valid_moves = [pos for pos in moves if
+                   pos.x >= 0 and pos.x < size and
+                   pos.y >= 0 and pos.y < size and
+                   pos not in visited]
+    # print("valid_moves: {}".format(valid_moves))
+
+    return valid_moves
+
+
+def run_knights_tour(start, size, visited):
+    # print("visited: {}".format(visited))
+    if len(visited) == size * size:
+        return 1
+
+    moves = get_potential_knight_moves(start, size, visited)
+
+    count = 0
+    for move in moves:
+        tmp_visted = visited.copy()
+        tmp_visted.add(move)
+        count += run_knights_tour(move, size, tmp_visted)
+
+    return count
+
+
+def count_knights_tours(size):
+    count = 0
+    for i in range(size):
+        for j in range(size):
+            start = Position(i, j)
+            print("start: {}".format(start))
+            count += run_knights_tour(start, size, set([start]))
+            print("count: {}".format(count))
+
+    print(count)
+    return count
+
+
+# assert count_knights_tours(1) == 1
+# assert count_knights_tours(2) == 0
+# assert count_knights_tours(3) == 0
+# assert count_knights_tours(4) == 0
+assert count_knights_tours(5) == 1728
