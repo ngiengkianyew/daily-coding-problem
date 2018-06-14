d91b8f97903755a34cb6728f4f0f1ec247052ef7
Vineet John <iamvineetjohn@gmail.com>
2018-06-14 12:31:30 -0400

    Add solution to Problem 89

--- README.md
+++ README.md
@@ -1420,3 +1420,15 @@ Implement division of two positive integers without using the division, multipli
 [Solution](solutions/problem_88.py)
 
 ---
+
+#### Problem 89
+
+This problem was asked by LinkedIn.
+
+Determine whether a tree is a valid binary search tree.
+
+A binary search tree is a tree with two children, left and right, and satisfies the constraint that the key in the left child must be less than or equal to the root and the key in the right child must be greater than or equal to the root.
+
+[Solution](solutions/problem_89.py)
+
+---
---
+++ solutions/problem_89.py
@@ -0,0 +1,70 @@
+import sys
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def is_valid_bst_node_helper(node, lb, ub):
+
+    if node and node.val <= ub and node.val >= lb:
+        return is_valid_bst_node_helper(node.left, lb, node.val) and \
+            is_valid_bst_node_helper(node.right, node.val, ub)
+
+    return not node  # if node is None, it's a valid BST
+
+
+def is_valid_bst_node(root):
+    return is_valid_bst_node_helper(root, -sys.maxsize, sys.maxsize)
+
+
+# Tests
+
+
+assert is_valid_bst_node(None)
+
+a = Node(3)
+b = Node(2)
+c = Node(6)
+d = Node(1)
+e = Node(3)
+f = Node(4)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.left = f
+assert is_valid_bst_node(a)
+
+
+a = Node(1)
+b = Node(2)
+c = Node(6)
+d = Node(1)
+e = Node(3)
+f = Node(4)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.left = f
+assert not is_valid_bst_node(a)
+
+a = Node(3)
+b = Node(2)
+c = Node(6)
+d = Node(1)
+e = Node(4)
+f = Node(4)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.left = f
+assert not is_valid_bst_node(a)
