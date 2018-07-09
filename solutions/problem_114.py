eefb823bd4e8898ed316823a19fe42ec021aaf94
Vineet John <iamvineetjohn@gmail.com>
2018-07-09 13:26:15 -0400

    Improve readability of solution

--- solutions/problem_114.py
+++ solutions/problem_114.py
@@ -1,7 +1,7 @@
 def reverse_words(string, delimiters):
     words = list()
     delims = list()
-    delim_indices = list()
+    delim_positions = list()  # stores positions of the delimiters seen
 
     start = 0
     i = 0
@@ -13,26 +13,30 @@ def reverse_words(string, delimiters):
             if i - start > 1:
                 words.append(word)
             delims.append(char)
-            delim_indices.append(len(words) + len(delims) - 1)
+            delim_positions.append(len(words) + len(delims) - 1)
             start = i + 1
 
         i += 1
 
+    # get last word if present
     if i - start > 1:
         words.append(string[start:i])
 
-    words.reverse()
+    words.reverse()  # reverse just the words
 
     reversed_order = list()
-    wi = 0
-    di = 0
+    word_index = 0
+    delim_index = 0
+
+    # merging the reversed words and the delimiters
     for i in range(len(words) + len(delims)):
-        if di < len(delim_indices) and delim_indices[di] == i:
-            reversed_order.append(delims[di])
-            di += 1
+        if delim_index < len(delim_positions) and delim_positions[delim_index] == i:
+            # insert next delimiter if the position is saved for a delimiter
+            reversed_order.append(delims[delim_index])
+            delim_index += 1
         else:
-            reversed_order.append(words[wi])
-            wi += 1
+            reversed_order.append(words[word_index])
+            word_index += 1
 
     reversed_string = "".join(reversed_order)
 
