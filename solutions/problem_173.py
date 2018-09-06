f2554f317763eda4790db5bca0212a49b2592566
Vineet John <iamvineetjohn@gmail.com>
2018-09-06 16:53:48 -0700

    Add solution to Problem 173

---
+++ solutions/problem_173.py
@@ -0,0 +1,38 @@
+def is_dict(var):
+    return str(type(var)) == "<class 'dict'>"
+
+
+def flatten_helper(d, flat_d, path):
+    if not is_dict(d):
+        flat_d[path] = d
+        return
+
+    for key in d:
+        new_keypath = "{}.{}".format(path, key) if path else key
+        flatten_helper(d[key], flat_d, new_keypath)
+
+
+def flatten(d):
+    flat_d = dict()
+    flatten_helper(d, flat_d, "")
+    print(flat_d)
+    return flat_d
+
+
+# Tests
+
+d = {
+    "key": 3,
+    "foo": {
+        "a": 5,
+        "bar": {
+            "baz": 8
+        }
+    }
+}
+
+assert flatten(d) == {
+    "key": 3,
+    "foo.a": 5,
+    "foo.bar.baz": 8
+}
