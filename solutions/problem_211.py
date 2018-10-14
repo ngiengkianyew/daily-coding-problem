d03a053b88b0d00aef45296eb0fa93029450355d
Vineet John <iamvineetjohn@gmail.com>
2018-10-14 10:37:59 -0700

    Add solution to Problem 211

---
+++ solutions/problem_211.py
@@ -0,0 +1,17 @@
+def get_occurrences(string, pattern):
+    sl, pl = len(string), len(pattern)
+    occurrences = list()
+
+    for i in range(sl - pl + 1):
+        if string[i:i+pl] == pattern:
+            occurrences.append(i)
+
+    print(occurrences)
+    return occurrences
+
+
+# Tests
+assert get_occurrences("abracadabra", "abr") == [0, 7]
+assert not get_occurrences("abracadabra", "xyz")
+assert not get_occurrences("abr", "abracadabra")
+assert get_occurrences("aaaa", "aa") == [0, 1, 2]
