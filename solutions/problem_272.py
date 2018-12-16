da36b04a96c79f7e17fed8339ceeea3c364e05d2
Vineet John <iamvineetjohn@gmail.com>
2018-12-16 11:23:32 -0800

    Optimize solution

--- solutions/problem_272.py
+++ solutions/problem_272.py
@@ -1,16 +1,14 @@
-def throw_dice(num_dice, faces, total):
-    if num_dice == 0 or total < 1:
+def perm_counter(num_dice, face_range, total):
+    if num_dice < 1 or total < 1:
         return 0
-
-    face_range = range(1, faces + 1)
-    if num_dice == 1 and total in face_range:
+    elif num_dice == 1 and total in face_range:
         return 1
 
-    num_perms = 0
-    for i in face_range:
-        num_perms += throw_dice(num_dice - 1, faces, total - i)
+    return sum([perm_counter(num_dice - 1, face_range, total - x) for x in face_range])
 
-    return num_perms
+
+def throw_dice(num_dice, faces, total):
+    return perm_counter(num_dice, range(1, faces + 1), total)
 
 
 # Tests
