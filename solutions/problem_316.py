5f1053b3bc75ac515f4f04c9269531ae5834382b
Vineet John <iamvineetjohn@gmail.com>
2019-01-29 20:48:15 -0800

    Add solution to Problem 316

---
+++ solutions/problem_316.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+def get_ways_to_produce(num: int, factors: List[int]):
+    if not num or num in factors:
+        return 1
+
+    ways = 0
+    for i, factor in enumerate(factors):
+        if num % factor == 0:
+            ways += get_ways_to_produce(num // factor, factors[i:])
+
+    return ways
+
+
+def get_denominators(ways_to_produce: List[int],
+                     known_denoms: List[int] = list()):
+    factors = [i for i, num in enumerate(ways_to_produce)
+               if (num == 1 and i > 0)]
+
+    for i, num in enumerate(ways_to_produce):
+        if get_ways_to_produce(i, factors) == num - 1:
+            factors.append(i)
+
+    return factors
+
+
+# Tests
+assert get_denominators([1, 0, 1, 1, 2]) == [2, 3, 4]
