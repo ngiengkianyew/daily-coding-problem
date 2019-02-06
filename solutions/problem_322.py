d49f64d4e72dcb8318a81278adb7f1fe6272797e
Vineet John <iamvineetjohn@gmail.com>
2019-02-05 19:36:13 -0800

    Remedy incorrect solution

--- solutions/problem_322.py
+++ solutions/problem_322.py
@@ -3,12 +3,22 @@ def jump_to_target(num):
     if abs_num < 2:
         return abs_num
 
-    return 1 + (2 * (abs_num - 1))
+    point, new_point = None, 0
+    jump = 1
+    while new_point <= abs_num:
+        point = new_point
+        new_point += jump
+        jump += 1
+    jump -= 2
+
+    return (2 * (abs_num - point)) + jump
 
 
 # Tests
 assert jump_to_target(0) == 0
 assert jump_to_target(1) == 1
 assert jump_to_target(2) == 3
-assert jump_to_target(3) == 5
-assert jump_to_target(-3) == 5
+assert jump_to_target(3) == 2
+assert jump_to_target(-3) == 2
+assert jump_to_target(10) == 4
+assert jump_to_target(11) == 6
